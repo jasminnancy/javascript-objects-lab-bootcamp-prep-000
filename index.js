f5a5f0e75a74c6dbcb374c01f8a5f77d22232897
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-07-19 17:32:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ function deleteFromObjectByKey(object, key) {
   delete newObj[key]
   return newObj
 }
-function 
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
