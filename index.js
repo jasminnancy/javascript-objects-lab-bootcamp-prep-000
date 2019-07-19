e7e3647f3de0d67a282cdbba35cf1571c3a3ce0b
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-07-19 17:31:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object)
-  delete 
+  delete newObj[key]
+  return newObj
 }
