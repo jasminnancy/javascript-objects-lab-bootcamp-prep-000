0991c0ab6030e079ef478c7f70e1a9f2d771164c
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-07-19 17:14:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var recipes = {}
-function updateObjectWithKeyAndValute(object, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) {
   recipes[key] = value
   return recipes
 }
