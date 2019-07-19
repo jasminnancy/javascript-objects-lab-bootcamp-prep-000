b7f8472f5e7dd84c245f630c323ca3f3878e4407
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-07-19 17:21:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
 }
-function 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
